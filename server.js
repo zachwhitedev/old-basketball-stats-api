75e655ae1a9db90dca611bae756de1ae4273f555
Zach White <zechnwhite@gmail.com>
2020-02-23 00:03:28 -0800

    redploy. adds LB for HTTPS

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}....`);
+  console.log(`server listening on port ${PORT}...`);
 });
