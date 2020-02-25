68bf411b7f77f7e861f7106e5c30ae4d8bc8612b
Zach White <zechnwhite@gmail.com>
2020-02-24 22:54:10 -0800

    seeing if status 200 allows response catch in login

--- ratelimits.js
+++ ratelimits.js
@@ -5,7 +5,7 @@ module.exports = {
     windowMs: 20 * 60 * 1000, // 20 minutes
     max: 500, // max requests allowed per windowMS
     message: {
-    //   status: 429,
+      //   status: 429,
       error:
         'You are making too many requests. Please wait 30 minutes and try again.'
     }
@@ -14,21 +14,20 @@ module.exports = {
     windowMs: 5 * 60 * 1000,
     max: 20,
     message: {
-      error:
-        'You are doing that too much. Please try again in 10 minutes.'
+      error: 'You are doing that too much. Please try again in 10 minutes.'
     }
   }),
   register: rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 20,
     message: {
-      error:
-        'You are doing that too much. Please try again in 20 minutes.'
+      error: 'You are doing that too much. Please try again in 20 minutes.'
     }
   }),
   test: rateLimit({
     windowMs: 1 * 60 * 1000,
     max: 5,
+    statusCode: 200,
     message: {
       status: 429,
       error: 'You are making too many requests. The rate limit is working!'
