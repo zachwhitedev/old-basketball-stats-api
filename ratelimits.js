9d61da78907cd1e439e5a72f2184b500e326b53f
Zach White <zechnwhite@gmail.com>
2020-02-24 22:19:58 -0800

    remove status code from rate limit error body

--- ratelimits.js
+++ ratelimits.js
@@ -5,7 +5,7 @@ module.exports = {
     windowMs: 20 * 60 * 1000, // 20 minutes
     max: 500, // max requests allowed per windowMS
     message: {
-      status: 429,
+    //   status: 429,
       error:
         'You are making too many requests. Please wait 30 minutes and try again.'
     }
@@ -14,7 +14,6 @@ module.exports = {
     windowMs: 5 * 60 * 1000,
     max: 20,
     message: {
-      status: 429,
       error:
         'You are making too many login attempts. Please try again in 10 minutes.'
     }
@@ -23,7 +22,6 @@ module.exports = {
     windowMs: 15 * 60 * 1000,
     max: 20,
     message: {
-      status: 429,
       error:
         'You are making too many register requests. Please try again in 20 minutes.'
     }
@@ -32,7 +30,6 @@ module.exports = {
     windowMs: 1 * 60 * 1000,
     max: 5,
     message: {
-      status: 429,
       error: 'You are making too many requests. The rate limit is working!'
     }
   })
